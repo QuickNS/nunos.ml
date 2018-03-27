b97b4e2b2ec184849caea51f65d188b0a20280cc
QuickNS <31905265+QuickNS@users.noreply.github.com>
2018-03-27 14:46:15 +0100

    added setup file

---
+++ Deep Learning Tutorial/setup.py
@@ -0,0 +1,59 @@
+import zipfile
+import sys, os, tempfile, logging
+import urllib.request as urllib2
+import urllib.parse as urlparse
+
+def download_file(url, dest=None):
+    """ 
+    Download and save a file specified by url to dest directory,
+    """
+    u = urllib2.urlopen(url)
+
+    scheme, netloc, path, query, fragment = urlparse.urlsplit(url)
+    filename = os.path.basename(path)
+    if not filename:
+        filename = 'downloaded.file'
+    if dest:
+        filename = os.path.join(dest, filename)
+
+    with open(filename, 'wb') as f:
+        meta = u.info()
+        meta_func = meta.getheaders if hasattr(meta, 'getheaders') else meta.get_all
+        meta_length = meta_func("Content-Length")
+        file_size = None
+        if meta_length:
+            file_size = int(meta_length[0])
+        print("Downloading: {0} Bytes: {1}".format(url, file_size))
+
+        file_size_dl = 0
+        block_sz = 8192
+        while True:
+            buffer = u.read(block_sz)
+            if not buffer:
+                break
+
+            file_size_dl += len(buffer)
+            f.write(buffer)
+
+            status = "{0:16}".format(file_size_dl)
+            if file_size:
+                status += "   [{0:6.2f}%]".format(file_size_dl * 100 / file_size)
+            status += chr(13)
+            print(status, end="")
+        print()
+
+    return filename
+
+url = "https://nunosdatasets.blob.core.windows.net/animals/animals.zip"
+
+print("[INFO] Downloading dataset...")
+
+download_file(url)
+
+print("[INFO] Extracting images...")
+with zipfile.ZipFile('animals.zip', "r") as z:
+    z.extractall(".")
+
+os.remove('animals.zip')
+
+print("[INFO] Done!")
