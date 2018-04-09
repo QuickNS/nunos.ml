9f828d6c79b948127b0b7cb5e9f40992fdaa0b2f
Nuno Silva <nunos@microsoft.com>
2018-04-09 16:10:05 +0200

    added stuff

--- OpenCV Tutorial/utils/imutils.py
+++ OpenCV Tutorial/utils/imutils.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import cv2
+from urllib.request import urlopen
 
 def translate(image, x, y):
 	# Define the translation matrix and perform the translation
@@ -12,7 +13,7 @@ def translate(image, x, y):
 	# Return the translated image
 	return shifted
 
-def rotate(image, angle, center = None, scale = 1.0):
+def rotate(image, angle, center=None, scale=1.0):
 	# Grab the dimensions of the image
 	(h, w) = image.shape[:2]
 
@@ -28,7 +29,7 @@ def rotate(image, angle, center = None, scale = 1.0):
 	# Return the rotated image
 	return rotated
 
-def resize(image, width = None, height = None, inter = cv2.INTER_AREA):
+def resize(image, width=None, height=None, inter=cv2.INTER_AREA):
 	# initialize the dimensions of the image to be resized and
 	# grab the image size
 	dim = None
@@ -59,7 +60,7 @@ def resize(image, width = None, height = None, inter = cv2.INTER_AREA):
 	# return the resized image
 	return resized
 
-def plot_histogram(image, title, mask = None):
+def plot_histogram(image, title, mask=None):
     # Grab the image channels, initialize the tuple of colors
 	# and the figure
 
@@ -79,13 +80,23 @@ def plot_histogram(image, title, mask = None):
 		plt.plot(hist, color = color)
 		plt.xlim([0, 256])
 
-def displayImage(image, shiftColor=True, figSize=(8,8), title=None):
+def displayImage(image, figSize=(8,8), title=None):
     fig = plt.figure(figsize=figSize)
     plt.axis("off")
-    if shiftColor:
+    if len(image.shape) == 3:
         plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
     else:
         plt.imshow(image, cmap='Greys_r',  interpolation='bicubic')
     if title is not None:
         plt.title(title)
     plt.show()
+
+def url_to_image(url, readFlag=cv2.IMREAD_COLOR):
+    # download the image, convert it to a NumPy array, and then read
+    # it into OpenCV format
+    resp = urlopen(url)
+    image = np.asarray(bytearray(resp.read()), dtype="uint8")
+    image = cv2.imdecode(image, readFlag)
+
+    # return the image
+    return image
