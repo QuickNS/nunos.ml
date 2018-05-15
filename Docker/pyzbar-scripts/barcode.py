928fc8f92e8a0a607fbfa73c3e25969b5d0f4686
QuickNS <31905265+QuickNS@users.noreply.github.com>
2018-05-15 01:34:00 +0100

    added barcode detection

---
+++ Docker/pyzbar-scripts/barcode.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+import pyzbar.pyzbar as pyzbar
+import numpy as np
+import cv2
+from imutils import resize
+
+def decode(im):
+    # Find barcodes and QR codes
+    decodedObjects = pyzbar.decode(im)
+    return decodedObjects
+
+def readBarcode(image):
+    decodedObjects = decode(image)
+    barcodes =[]
+    for obj in decodedObjects:
+        barcodes.append(obj.data.decode('ascii'))
+    return barcodes
+    
+im = cv2.imread('barcode_02.jpg')
+
+barcodes = np.array([])
+for i in [200, 500, 1000]:
+    t = resize(im, width=i)
+    barcodes= np.unique(np.concatenate((barcodes, readBarcode(t))))
+
+print(barcodes)
